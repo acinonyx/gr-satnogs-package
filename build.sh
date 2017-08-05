88698c1fefc15992c940fc0c76d2be39280ff3ff
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2017-08-05 17:08:31 +0300

    Mount output directory for packages

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
+DEBIAN_DIR="/tmp/debian"
+WORKING_DIR="/tmp/dpkg"
+
+# Change to working directory
+cd "$WORKING_DIR"
+
 # Get original source code
-debian/rules get-orig-source
+"$DEBIAN_DIR"/rules get-orig-source
 
 # Untar original source code
 tar -xvf gr-satnogs*.orig.tar.xz
 
 # Copy debian packaging directory
-cp -R debian gr-satnogs*/
+cp -R "$DEBIAN_DIR" gr-satnogs*/
 
 # Build package
 cd gr-satnogs*/
